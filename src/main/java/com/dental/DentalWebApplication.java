e7eed855f1c95d82dc143c7e356b58d9f36796e3
Mostafa <m.teba1991@gmail.com>
2018-04-20 15:08:48 +0200

    ddd

--- src/main/java/com/dental/DentalWebApplication.java
+++ src/main/java/com/dental/DentalWebApplication.java
@@ -1,12 +1,13 @@
-package com.example.demo;
+package com.dental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class DentalWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DentalWebApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(DentalWebApplication.class, args);
 	}
 }
---
+++ src/main/java/com/dental/PersonService.java
@@ -0,0 +1,8 @@
+package com.dental;
+
+import java.util.List;
+
+public interface PersonService {
+	public Person addPersone(Person person);
+	public List<Person> getAllPersons();
+}
